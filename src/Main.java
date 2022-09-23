afa565d41fe955609007d4acf7b882fa76a9d004
nkeeping1996 <nfk.keeping@gmail.com>
2022-09-22 21:52:40 -0400

    Initial attempt - still need to de bug

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+
+        while(i < m || j < n){
+            if(nums2[j] < nums1[i]){
+                for(int k = i+1; k < nums1.length-1; k++){
+                    nums1[k] = nums1[k+1];
+                }
+                nums1[i] = nums2[j];
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+
+    }
 }
